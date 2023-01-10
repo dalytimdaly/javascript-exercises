1979863e8f1d4ecda43d2bcef0d4826ec9d52560
Tim <dalytimdaly@gmail.com>
2023-01-10 16:44:22 -0700

    calculatored

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(a, b) {
+  let c = a + b
+	return c
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	let c = a - b
+  return c
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+    let sum = array.reduce((a,b) => a+b, 0)
+    return sum
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 0
+  for (let i = 0; i < array.length; i++) {
+    total = array.reduce((a,b) => a*b)
+  }
+  return total
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let total = a
+  for (let i = 1; i < b; i++) {
+    total = total * a
+  }
+  return total
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let factor = a
+  if (factor < 0) {
+    return null
+  } else if (factor === 0) {
+    return 1
+  } else if (factor === 1) {
+    return factor
+  } else if (factor > 1) {
+    return (factor * factorial(factor - 1))
+  } else
+    return 'fuck'
 };
 
 // Do not edit below this line
