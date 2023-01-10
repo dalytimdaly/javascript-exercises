39ffcdaf4055fa7b2c3e2ac3654b77de2a58746c
Tim <dalytimdaly@gmail.com>
2023-01-10 10:52:23 -0700

    2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
 
-};
+
+const repeatString = function(string, num) {
+  let array = []
+  if (num < 0) {
+    return "ERROR"
+  } 
+  for (let i = 0; i < num; i++) {
+    array.push(string)
+  }
+  let result = array.toString()
+  let repeatedString = result.replaceAll(",", "")
+  return repeatedString
+}
+
 
 // Do not edit below this line
 module.exports = repeatString;
