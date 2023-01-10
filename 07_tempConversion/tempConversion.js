d51e4c0f41672873f423d43776c69265111c8add
Tim <dalytimdaly@gmail.com>
2023-01-10 15:45:08 -0700

    temperatures converted

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheitTemp) {
+  let firstTemp = farenheitTemp - 32
+  let secondTemp = firstTemp * 5
+  let celsiusTemp = secondTemp / 9
+  if (Number.isInteger(celsiusTemp)) {
+    return celsiusTemp
+  } else 
+    return parseFloat(celsiusTemp.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  let firstTemp = celsiusTemp * 9
+  let secondTemp = firstTemp / 5
+  let farenheitTemp = secondTemp + 32
+  if (Number.isInteger(farenheitTemp)) {
+    return farenheitTemp
+  } else
+    return parseFloat(farenheitTemp.toFixed(1))
 };
 
 // Do not edit below this line
