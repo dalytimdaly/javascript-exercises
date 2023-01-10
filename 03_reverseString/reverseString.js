1007ebfd6c5a44e7ffcd0ac6a252ec51a6d00064
Tim <dalytimdaly@gmail.com>
2023-01-10 11:05:43 -0700

    string reversed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
 
-};
+  const reverseString = function(string) {
+    let stringArray = string.split("")
+    let reverse = stringArray.reverse()
+    let reverseStrung = reverse.toString()
+    return reverseStrung.replaceAll(",", "")
+  };
+
 
 // Do not edit below this line
 module.exports = reverseString;
