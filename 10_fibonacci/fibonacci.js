8412c3bdfcc8cc2cfca1f5a90326fbada20cedbb
Tim <dalytimdaly@gmail.com>
2023-01-10 17:20:30 -0700

    fibonaccis

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
 
+
+const fibonacci = function(int) {
+  let integer = parseInt(int)
+  let a = 1
+  let b = 1
+  if (int < 0) {
+    return "OOPS"
+  } else
+  for (let i = 0; i < integer - 2; i++) {
+    let c = a + b
+    a = b
+    b = c 
+  }
+  return b
 };
 
 // Do not edit below this line
