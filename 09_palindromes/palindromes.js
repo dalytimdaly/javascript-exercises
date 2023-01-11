4d3c82a76df97c5653b52c741effbb376e7b7a96
Tim <dalytimdaly@gmail.com>
2023-01-10 17:07:51 -0700

    palindrome emordnilap

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let lowerString = string.toLowerCase()
+  let replacePunctuation = lowerString.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+  let removeSpaces = replacePunctuation.replace(/\s{2,}/g, " ")
+  let removeSpaces2 = removeSpaces.replaceAll(" ", "")
+  let stringArr = replacePunctuation.split("")
+  let reversed = stringArr.reverse()
+  let reversedString = reversed.join("")
+  let reversedStringToo = reversedString.replaceAll(" ", "")
+  if (reversedStringToo.toLowerCase() === removeSpaces2) {
+    return true
+  } else return false
 };
 
 // Do not edit below this line
