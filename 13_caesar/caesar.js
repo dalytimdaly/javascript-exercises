404d7c5c76d1691faa14ea3ae09b948b111f6b50
Tim <dalytimdaly@gmail.com>
2023-01-11 18:34:51 -0700

    pretty far

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,16 @@
-const caesar = function() {
-
+const caesar = function(string, number) {
+  let stringArr = Array.from(string)
+  let unicodedArr = stringArr.map((char) => char.codePointAt(0))
+  let crypticArr = unicodedArr.map((num) => num + number)
+  let decodedArr = crypticArr.map((int) => String.fromCharCode(int))
+  console.log(stringArr)
+  console.log(decodedArr)
+  for (let i = 0; i < decodedArr.length; i++) {
+    
+  }
+  let decodedStr = decodedArr.toString().replaceAll(",", "")
+  return decodedStr
 };
 
-// Do not edit below this line
+// Do not edit below this line .replaceAll("1", ",").replaceAll("%", " ").replaceAll("\\", "B").replaceAll("&", "!")
 module.exports = caesar;
